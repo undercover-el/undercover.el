9f755e4824a0a91bec2c5f3b0c3e5758bf88e089
Sviridov Alexander <sviridov.vmi@gmail.com>
2014-09-27 14:14:51 +0600

    Added: library skeleton

---
+++ Cask
@@ -0,0 +1,9 @@
+(source gnu)
+(source melpa)
+
+(package-file "undercover.el")
+
+(files "*.el")
+
+(development
+ (depends-on "ert-runner"))
---
+++ undercover.el
@@ -0,0 +1,18 @@
+;;; undercover.el --- Test coverage library for Emacs -*- lexical-binding: t -*-
+
+;; Copyright (c) 2014 Sviridov Alexander
+
+;; Author: Sviridov Alexander <sviridov.vmi@gmail.com>
+;; URL: https://github.com/sviridov/undercover.el
+;; Created: Sat Sep 27 2014
+;; Keywords: lisp, tests, coverage, tools
+;; Version: 0.0.1
+
+;;; Commentary:
+
+;; Provides a test coverage tools for Emacs packages.
+
+;;; Code:
+
+(provide 'undercover)
+;;; undercover.el ends here
